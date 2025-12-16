7d59672f6422021b5c61e0a2179c1f6b1f1d4fe0
Tajim <com.tajim@gmail.com>
2025-12-16 12:32:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android_worker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android_worker;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
